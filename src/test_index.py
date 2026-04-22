0810b5ba7a48e04eb3ee273db822ec3cbe095729
rwzimmerman04 <rwzimmerman04@gmail.com>
2026-04-22 16:12:23 -0700

    Update test_index.py to use new index_documents() signature

--- src/test_index.py
+++ src/test_index.py
@@ -6,15 +6,18 @@ from indexer import index_documents
 from searcher import search_index
 
 INDEX_PATH = "../.index"
-FOLDER_PATH = "../tests"
+# FILE_PATH = "../tests/Rummage_test.pdf"         # Use for testing single file search
+FILE_PATH = "../tests"                        # Use for testing folder search and also errored file search
+# MODE = "file"                                   # Use for testing single file search
+MODE = "recursive"                            # Use for testing folder search and also errored file search
 QUERY = "holy knight"
 
 # Build the index
-idx = index_documents(FOLDER_PATH, INDEX_PATH)
+idx = index_documents(FILE_PATH, INDEX_PATH, MODE)
 print("Completed Indexing")
 
 # Search the index
-matches = search_index("holy knight", "../.index")
+matches = search_index(QUERY, INDEX_PATH)
 print(f"Found {len(matches)} results:")
 
 # Report the results
