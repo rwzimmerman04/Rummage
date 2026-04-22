e01a31939b5af5a260e9edfc364d9f8e162923a0
rwzimmerman04 <rwzimmerman04@gmail.com>
2026-04-22 15:27:15 -0700

    Update comments and add build test to test_index.py

--- src/test_index.py
+++ src/test_index.py
@@ -1,7 +1,22 @@
-from searcher import search_index
-matches = search_index("holy knight", "../.index")
-    
+# test_index.py
+# Quick test check for the indexer and searcher
+# ===================================================
 
+from indexer import index_documents
+from searcher import search_index
+
+INDEX_PATH = "../.index"
+FOLDER_PATH = "../tests"
+QUERY = "holy knight"
+
+# Build the index
+idx = index_documents(FOLDER_PATH, INDEX_PATH)
+print("Completed Indexing")
+
+# Search the index
+matches = search_index("holy knight", "../.index")
 print(f"Found {len(matches)} results:")
+
+# Report the results
 for m in matches:
     print(m["filename"], m["page"], m["snippet"])
